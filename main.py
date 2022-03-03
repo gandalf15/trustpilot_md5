5f7e823fba01670421225ec470cf288ff42ea28e
marcel <marcel@optiplex-3020-pop-os.lan>
2022-03-03 19:45:33 +0000

    Solved the problem.

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 import hashlib
 import collections
 import pathlib
-from typing import Dict
+import time
+from typing import Dict, Tuple
+import trie
+import multiprocessing as mp
 
 
 def load_word_dict(word_dict_path: pathlib.Path,
-                   max_len: int,
+                   max_word_len: int,
                    only_ascii: bool = True,
                    only_alpha: bool = True) -> collections.OrderedDict:
     word_dict = collections.OrderedDict()
@@ -14,7 +17,7 @@ def load_word_dict(word_dict_path: pathlib.Path,
         for line in f:
             word = line.strip()
             word = word.lower()
-            if len(word) > max_len: continue
+            if len(word) > max_word_len: continue
             if only_ascii and word.isascii():
                 if only_alpha and word.isalpha():
                     word_dict[word] = True
@@ -27,42 +30,122 @@ def load_word_dict(word_dict_path: pathlib.Path,
                     word_dict[word] = True
     return word_dict
 
+
 def contains_letters(string_1: str, string_2: str) -> bool:
     anagram_counter = collections.Counter(string_2)
-    for char in string_1:
-        if char in anagram_counter and anagram_counter[char] > 0:
-            anagram_counter[char] -= 1
+    for letter in string_1:
+        if letter in anagram_counter and anagram_counter[letter] > 0:
+            anagram_counter[letter] -= 1
         else:
             return False
     return True
 
-def remove_impossible_words(word_dict: collections.OrderedDict, anagram: str) -> collections.OrderedDict:
+
+def remove_impossible_words(word_dict: collections.OrderedDict,
+                            anagram: str) -> collections.OrderedDict:
     new_word_dict = collections.OrderedDict()
     for word in word_dict.keys():
         if contains_letters(word, anagram):
-            new_word_dict[word] = collections.Counter(word)
+            new_word_dict[word] = True
     return new_word_dict
 
+
+def check_hash(anagrams_queue: mp.Queue, target_hashes: Dict):
+    remaining_targets = list(target_hashes.keys())
+    counter = 0
+    start_time = time.time()
+    while len(remaining_targets) > 0:
+        anagram = anagrams_queue.get()
+        anagrams_queue.task_done()
+        if anagram is None:
+            break
+        counter += 1
+        if counter % 5000 == 0:
+            print("Checked:", counter, "anagrams but haven't found yet!")
+        md5_hash = hashlib.md5(anagram.encode('utf-8'))
+        md5_hash_hex = md5_hash.hexdigest()
+        if md5_hash_hex in target_hashes:
+            found_time = time.time() - start_time
+            target_hashes[md5_hash_hex] = (anagram, counter, found_time)
+            remaining_targets.remove(md5_hash_hex)
+            print("FOUND after {} sec:".format(found_time))
+            print(md5_hash_hex, "->", "'" + anagram + "'")
+    if len(remaining_targets) > 0:
+        print(
+            "Searched:", counter,
+            "anagrams but did not find all. Try different number of spaces.")
+        print("*" * 80)
+        for key, value in target_hashes.items():
+            print(key, "->", value)
+            print("=" * 80)
+    else:
+        print("Found All targets!")
+
+
+def load_task(task_path: pathlib.Path) -> Tuple[str, Dict]:
+    anagram = ""
+    target_hashes = {}
+    with open(task_path, "r") as f:
+        first_line = f.readline()
+        anagram = first_line.strip()
+        for line in f:
+            hash = line.strip()
+            if not hash == "":
+                target_hashes[hash] = None
+    return (anagram, target_hashes)
+
+
 def main():
-    anagram = "poultry outwits ants".replace(" ", "")
+    current_dir_path = pathlib.Path().resolve()
+    task_path = current_dir_path.joinpath("task")
+
+    anagram, target_hashes = load_task(task_path)
+    print("Anagram: ", anagram),
+    print("Target hashes:")
+    for hash in target_hashes.keys():
+        print(hash)
+    number_of_spaces_in_anagram = anagram.count(" ")
+    anagram_without_spaces = anagram.replace(" ", "")
+    prompt_message = ("Anagram has {0} spaces. "
+                      "How many spaces do you want? (default: {0}): "
+                      ).format(number_of_spaces_in_anagram)
+    usr_input = input(prompt_message)
+    if usr_input == "":
+        wanted_spaces = number_of_spaces_in_anagram
+    elif usr_input.isdigit():
+        wanted_spaces = int(usr_input)
+    else:
+        raise ValueError(
+            "Number of spaces must be a digit. You wrote '{}'.".format(
+                usr_input))
+
     only_ascii = False
     only_alpha = False
-    max_len = len(anagram)
-    if anagram.isascii():
+    anagram_len = len(anagram_without_spaces)
+    if anagram_without_spaces.isascii():
         only_ascii = True
-    if anagram.isalpha():
+    if anagram_without_spaces.isalpha():
         only_alpha = True
-    current_dir = pathlib.Path().resolve()
-    word_dict_path = current_dir.joinpath("wordlist")
-    word_dict = load_word_dict(word_dict_path, max_len, only_ascii, only_alpha)
-    print(len(word_dict))
-    print(list(word_dict.keys())[len(word_dict) - 10:])
+    wordlist_path = current_dir_path.joinpath("wordlist")
 
-    word_dict = remove_impossible_words(word_dict, anagram)
-    print(word_dict)
-    print(len(word_dict))
+    word_dict = load_word_dict(wordlist_path, anagram_len, only_ascii,
+                               only_alpha)
+    word_dict = remove_impossible_words(word_dict, anagram_without_spaces)
+    wordlist_trie = trie.Trie(list(word_dict.keys()))
 
-
+    anagrams_queue = mp.Manager().Queue()
+    anagram_process_args = (anagram_without_spaces, wanted_spaces,
+                            anagrams_queue)
+    anagrams_process = mp.Process(target=wordlist_trie.anagrams,
+                                  args=anagram_process_args)
+    check_hash_args = (anagrams_queue, target_hashes)
+    check_hash_process = mp.Process(target=check_hash, args=check_hash_args)
+    anagrams_process.start()
+    check_hash_process.start()
+    print("Search started.")
+    check_hash_process.join()
+    if anagrams_process.is_alive():
+        anagrams_process.terminate()
 
 
 if __name__ == "__main__":
--- trie.py
+++ trie.py
@@ -1,7 +1,9 @@
 """
 This file hold Trie Class
 """
+import multiprocessing
 from typing import List, Dict
+import queue
 
 
 class Trie:
@@ -23,16 +25,17 @@ class Trie:
             node = node[letter]
         node[self.end_symbol] = True
 
-    def anagrams(self, string: str, max_spaces: int):
-        result = []
-        self._anagrams_helper(string, "", max_spaces, self.root, result)
-        return result
+    def anagrams(self, string: str, max_spaces: int,
+                 results_queue: multiprocessing.Queue) -> None:
+        self._anagrams_helper(string, "", max_spaces, self.root, results_queue)
+        results_queue.put(None)
 
     def _anagrams_helper(self, string: str, prefix: str, remaining_spaces: int,
-                         node: Dict, result: List[str]) -> None:
-        if not string:
+                         node: Dict,
+                         results_queue: multiprocessing.Queue) -> None:
+        if not string and remaining_spaces == 0:
             if self.end_symbol in node:
-                result.append(prefix)
+                results_queue.put(prefix)
             return
 
         for node_letter, child in node.items():
@@ -40,13 +43,13 @@ class Trie:
                 new_string = string.replace(node_letter, "", 1)
                 new_prefix = prefix + node_letter
                 self._anagrams_helper(new_string, new_prefix, remaining_spaces,
-                                      child, result)
+                                      child, results_queue)
 
         if self.end_symbol in node and remaining_spaces > 0:
             remaining_spaces -= 1
             prefix += " "
             self._anagrams_helper(string, prefix, remaining_spaces, self.root,
-                                  result)
+                                  results_queue)
 
     def __repr__(self) -> str:
         return str(self.root)
