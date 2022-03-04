1d9c3dfe1a8c56c058cfce03b593fa320d426284
Marcel Zak <marcel@zak.codes>
2022-03-04 16:57:58 +0000

    Add docstring for Trie and tidy up.

--- trie.py
+++ trie.py
@@ -1,55 +1,91 @@
 """
-This file hold Trie Class
+This file hold Trie class.
 """
-import multiprocessing
-from typing import List, Dict
-import queue
+
+import multiprocessing as mp
+
+# define Node type
+Node = dict[str, dict]
 
 
 class Trie:
+    """
+    Trie represents a trie data structure.
 
-    def __init__(self, wordlist: List[str]) -> None:
-        self.root = {}
-        self.end_symbol = "*"
-        self.populate_trie_from(wordlist)
+    Trie is an efficient information reTrieval data structure. Using Trie,
+    search complexities can be brought to optimal limit (key length).
+    However, Trie can be also used for efficient anagram generation.
+    """
 
-    def populate_trie_from(self, wordlist: List[str]) -> None:
+    def __init__(self, wordlist: list[str]) -> None:
+        """
+        Init a new instance of a Trie.
+
+        Args:
+            wordlist: List of words to store.
+        """
+        self._root = {}  # Node is a simple dict.
+        self._end_symbol = "*"  # Word terminating character.
+        self._populate_trie_from(wordlist)
+
+    def _populate_trie_from(self, wordlist: list[str]) -> None:
+        """
+        Populates the trie from wordlist.
+
+        Args:
+            wordlist: List of words to store.
+        """
         for word in wordlist:
             self.add_word(word)
 
     def add_word(self, word: str):
-        node = self.root
+        """
+        Adds one word to the trie.
+
+        Args:
+            word: The word to add.
+        """
+        node = self._root
         for letter in word:
             if letter not in node:
                 node[letter] = {}
             node = node[letter]
-        node[self.end_symbol] = True
+        node[self._end_symbol] = True
 
-    def anagrams(self, string: str, max_spaces: int,
-                 results_queue: multiprocessing.Queue) -> None:
-        self._anagrams_helper(string, "", max_spaces, self.root, results_queue)
+    def anagrams(self, letters: str, spaces: int,
+                 results_queue: mp.Queue) -> None:
+        """
+        Generates anagrams from given letters that exist in the trie.
+
+        Args:
+        letters: Letters for anagrams.
+        spaces: Number of spaces that the anagram should have.
+        results_queue: A queue where the generated anagrams are put.
+            After all anagrams are generated, it puts None in the queue 
+            to signalise that it has finished.
+        """
+        self._anagrams_helper(letters, "", spaces, self._root, results_queue)
         results_queue.put(None)
+        results_queue.close()
 
     def _anagrams_helper(self, string: str, prefix: str, remaining_spaces: int,
-                         node: Dict,
-                         results_queue: multiprocessing.Queue) -> None:
+                         node: Node, results_queue: mp.Queue) -> None:
+        # Base case when we exhausted all the letters and spaces.
+        # We have desired anagram.
         if not string and remaining_spaces == 0:
-            if self.end_symbol in node:
+            if self._end_symbol in node:
                 results_queue.put(prefix)
             return
-
-        for node_letter, child in node.items():
-            if node_letter in string:
-                new_string = string.replace(node_letter, "", 1)
-                new_prefix = prefix + node_letter
+        # DFS for all valid anagrams.
+        for child_letter, child_node in node.items():
+            if child_letter in string:
+                new_string = string.replace(child_letter, "", 1)
+                new_prefix = prefix + child_letter
                 self._anagrams_helper(new_string, new_prefix, remaining_spaces,
-                                      child, results_queue)
-
-        if self.end_symbol in node and remaining_spaces > 0:
+                                      child_node, results_queue)
+        # We have a valid word but we still have some spaces to use.
+        if self._end_symbol in node and remaining_spaces > 0:
             remaining_spaces -= 1
             prefix += " "
-            self._anagrams_helper(string, prefix, remaining_spaces, self.root,
+            self._anagrams_helper(string, prefix, remaining_spaces, self._root,
                                   results_queue)
-
-    def __repr__(self) -> str:
-        return str(self.root)
